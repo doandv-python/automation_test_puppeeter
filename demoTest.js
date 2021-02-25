21b62722bb73a6a2dd1fde41eed8c25610d27ceb
doandv-python <doandv.python@gmail.com>
2021-02-25 18:22:09 +0700

    demo test

---
+++ demoTest.js
@@ -0,0 +1,9 @@
+const puppeteer = require("puppeteer");
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.youtube.com/")
+    await browser.close();
+})();
